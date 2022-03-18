b56e4d64fe8dfc81a206c3e36cf27ccfdfea28e5
Sergio <sergiosilva@outlook.com>
2022-03-18 17:46:52 -0300

    Working on the code

---
+++ app.rb
@@ -0,0 +1,23 @@
+LOW = ("a".."z")
+CAPS = ("A".."Z")
+
+def ceasar_cipher(phrase, number)
+    new_array = []
+    phrase
+    if phrase.split("").include?
+        p phrase
+    end
+end
+
+ceasar_cipher("bubina", 1)
+
+
+
+# numbers = []
+# new_numbers = []
+# cipher = []
+# new_array = phrase.split("").to_a
+# new_array.each {|c| numbers << c.ord}
+# numbers.each {|n| new_numbers << n + number.to_i}
+# new_numbers.each {|n| cipher << n.chr}
+# p cipher.join()
